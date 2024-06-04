8f91721c37dcfe234aa134bc2fbcf9fe3d1a16e3
Bridget Galaty <bridgetgalaty@gmail.com>
2024-06-04 14:55:17 -0700

    Updated P2 to nonlinear

--- PS4/PS4_Problem2.py
+++ PS4/PS4_Problem2.py
@@ -10,9 +10,10 @@ gamma = 1
 varphi = 1
 chi = 1
 epsilon = 10
-rho = 0.8
+rho_a = 0.8
 phi_pi = 1.5
 phi_y = 0 
+mu = 1/(epsilon-1)
 theta_ = [0.0001, 0.25, 0.5, 0.75, 0.9999]
 lambda_ = []
 kappa_ = []
@@ -23,53 +24,61 @@ for i in range(len(theta_)):
     kappa_i = lambda_i*(gamma+varphi)
     kappa_.append(kappa_i)
 kappa = kappa_[0]
+theta = theta_[0]
 
 @simple
-# def nkpc(pi, yflex, beta, kappa):
-def nkpc(pi, yflex, beta, kappa):
-    y = yflex + 1 / kappa * (pi - beta * pi(+1))
-    # n = y - a
-    # return y, n
-    return y
+def hh(c, n, varphi, gamma, beta):
+    wp = chi*(n**varphi)/(c**(-gamma))
+    sdf = beta*(c**(-gamma))/(c(-1)**(-gamma))
+    r = 1/(beta*(c**(-gamma))/(c(-1)**(-gamma)))
+    return wp, sdf, r
+
+@simple
+def firm(a, pi, n, sdf, wp, theta, mu, epsilon):
+    y=a*n
+    f1 = (1+mu)*(a*n)*(wp/a)+theta*(pi(+1)**epsilon)*sdf(+1)*f1(+1)
+    f2 = (a*n)+theta*(pi(+1)**(epsilon-1))*sdf(+1)*f2(+1)
+    return y, f1, f2
+
+@simple
+def firm2(f1, f2):
+    pstar = f1/f2
+    return pstar
 
 @simple
 def central_bank(pi, v, phi_pi):
-    i = phi_pi * pi + v
-    return i
+    q = (1/beta)*(pi**phi_pi)*np.exp(v)
+    return q
 
 @simple
-def mkt_clearing(y, i, pi, gamma):
-    euler = gamma * y + i - pi(+1) - gamma * y(+1)
-    r = i - pi(+1)
-    c = y
-    return euler, r, c
+def mkt_clearing(y, c, pstar, q,r,pi):
+    output = y - c
+    inflation  = theta*(pi**(epsilon-1))+(1-theta)*(pstar**(1-epsilon)) - 1
+    fisher = r - q(-1)/(((1/theta)*(1-(1-theta)*(pstar**(1-epsilon))))**(1/(epsilon-1)))
+    return fisher, output, inflation
 
-
-print(nkpc)
-print(f"Inputs: {nkpc.inputs}")
-print(f"Outputs: {nkpc.outputs}")
-
-nk = create_model([nkpc, central_bank, mkt_clearing], name="NK")
+nk = create_model([hh, firm, firm2, central_bank, mkt_clearing], name="NK")
 
 print(nk)
 print(f"Blocks: {nk.blocks}")
 
 # steady state values
-calibration = {'yflex': 0, 'v': 0, 'y': 0, 'c': 0, 'r': 0, 'i': 0, 'gamma': gamma, 'beta': beta, 'phi_pi': phi_pi, 'kappa': kappa}
+# NEED TO CALCULATE THESE
+calibration = {'v': 0, 'a': 1, 'y': 0, 'r': 0, 'gamma': gamma, 'beta': beta, 'phi_pi': phi_pi, 'kappa': kappa, 'theta': theta}
 
 # solve for steady state (we know it, but running this routine helps us check for mistakes)
-unknowns_ss = {'pi': 0}
-targets_ss = { "euler": 0}
+unknowns_ss = {'n': 1, 'c':1, 'pi':1}
+targets_ss = { "fisher": 0, "output": 0, "inflation": 0}
 
 ss = nk.solve_steady_state(calibration, unknowns_ss, targets_ss, solver="broyden_custom")
 
 # checking that we are in the steady state that we expect
-assert np.allclose(ss['pi'], 0)
-assert np.allclose(ss['euler'], 0)
+assert np.allclose(ss['n', 'c', 'pi'], 0)
+assert np.allclose(ss['fisher'], 0)
 
-unknowns = ['pi']
-targets = ['euler']
-inputs = ['yflex', 'v']
+unknowns = ['n', 'c', 'pi']
+targets = ['fisher', 'output', 'inflation']
+inputs = ['a', 'v']
 
 G = nk.solve_jacobian(ss, unknowns, targets, inputs, T=300)
 
@@ -80,6 +89,7 @@ calibration_copies = {}
 for i in range(1,len(theta_)):
     calibration_i = calibration.copy()
     calibration_i['kappa'] = kappa_[i]
+    calibration_i['theta'] = theta_[i]
 
     # calculate new steady state
     ss_i = nk.solve_steady_state(calibration_i, unknowns_ss, targets_ss, solver="broyden_custom")
